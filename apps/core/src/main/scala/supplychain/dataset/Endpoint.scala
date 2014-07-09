baa1e3c699eaa5f073635ce3aa0ece4d8506a1bb
robertisele <mail@robertisele.com>
2014-07-09 13:12:18 +0200

    RemoteEndpoint: Add SPARQL update queries manually

--- apps/core/src/main/scala/supplychain/dataset/Endpoint.scala
+++ apps/core/src/main/scala/supplychain/dataset/Endpoint.scala
@@ -1,8 +1,11 @@
 package supplychain.dataset
 
-import com.hp.hpl.jena.query.{ResultSet, QueryExecutionFactory, QueryFactory}
+import java.io.OutputStreamWriter
+import java.net.{HttpURLConnection, URL, URLEncoder}
+
+import com.hp.hpl.jena.query.{QueryExecutionFactory, QueryFactory, ResultSet}
 import com.hp.hpl.jena.rdf.model.{Model, ModelFactory}
-import com.hp.hpl.jena.update.{UpdateExecutionFactory, UpdateFactory, UpdateAction}
+import com.hp.hpl.jena.update.UpdateAction
 
 /**
  * An RDF endpoint, which may either be a local or a remote endpoint.
@@ -35,8 +38,21 @@ class LocalEndpoint extends Endpoint {
 class RemoteEndpoint(endpointUrl: String) extends Endpoint {
 
   def update(query: String) = {
-    val parsedQuery = UpdateFactory.create(query)
-    UpdateExecutionFactory.createRemote(parsedQuery, endpointUrl).execute()
+    //Open a new HTTP connection
+    val url = new URL(endpointUrl)
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.setRequestMethod("POST")
+    connection.setDoOutput(true)
+    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+
+    // Write query
+    val writer = new OutputStreamWriter(connection.getOutputStream, "UTF-8")
+    writer.write("query=")
+    writer.write(URLEncoder.encode(query, "UTF-8"))
+    writer.close()
+
+    //val parsedQuery = UpdateFactory.create(query)
+    //UpdateExecutionFactory.createRemote(parsedQuery, endpointUrl).execute()
   }
 
   def select(query: String) = {
