2d1603d6d13e6c9a9f6ea648543b415d114321d5
rpietzsch <rpietzsch@gmail.com>
2014-09-02 17:22:32 +0200

    finishing case classes for json conversion and get impl

--- apps/simulator/src/main/scala/supplychain/simulator/WeatherNcdc.scala
+++ apps/simulator/src/main/scala/supplychain/simulator/WeatherNcdc.scala
@@ -1,41 +1,51 @@
 package supplychain.simulator
 
-import play.api.libs.json.{Reads, JsResult}
+import play.api.libs.json.{JsValue, Reads, JsResult}
 import play.api.libs.ws._
 import scala.concurrent.Future
 import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by rene on 01.09.14.
  */
 object WeatherNcdc {
 
-  def get(uri: String): String = {
+  def get(uri: String): Future[JsValue] = {
 
-    //implicit val context = scala.concurrent.ExecutionContext.Implicits.global
-
-    val holder: WSRequestHolder = WS.url(uri)
-    val complexHolder: WSRequestHolder = holder.withHeaders("token" -> "FHZLEZWsBusSZulVeicGwUzkXTCFTBIp")
+    val holder = WS.url(uri)
+    val complexHolder = holder.withHeaders("token" -> "FHZLEZWsBusSZulVeicGwUzkXTCFTBIp")
     val promise = complexHolder.get()
-    val body = promise.value.get.toString
-    return body
+
+    for (response <- promise) yield {
+      response.json
+    }
   }
 }
 
-case class NcdcLocation(
-						id: String,
-						elevation: Int,
-						name: String,
-						elevationUnit: String,
-						datacoverage: Double,
-						longitude: Double,
-						mindate: String,
-						latitude: Double,
-						maxdate: String)
+
+case class NcdcLocation(  id: String,
+                          elevation: Int,
+                          name: String,
+                          elevationUnit: String,
+                          datacoverage: Double,
+                          longitude: Double,
+                          mindate: String,
+                          latitude: Double,
+                          maxdate: String)
                          
-case class NcdcMetadata(
-						limit: Int,
-						count: Int,
-						offset: Int)
+case class NcdcMetadata(resultset: NcdcResultset)
+
+case class NcdcResultset (limit: Int, count: Int, offset: Int)
 
 case class NcdcLocationResults(results: List[NcdcLocation], metadata: NcdcMetadata)
+
+
+
+case class NcdcDailySummary(  station: String,
+                              value: Int,
+                              attributes: String,
+                              datatype: String,
+                              date: String)
+
+case class NcdcDailySummaryResult(results: List[NcdcDailySummary], metadata: NcdcMetadata)
