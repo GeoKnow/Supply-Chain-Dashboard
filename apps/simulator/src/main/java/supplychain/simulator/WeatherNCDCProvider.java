c41df79b549d4ada41eb36e81e74f6ac4faf9e35
rpietzsch <rpietzsch@gmail.com>
2014-09-02 13:48:54 +0200

    getting data from the ncdc REST API

---
+++ apps/simulator/src/main/java/supplychain/simulator/WeatherNCDCProvider.java
@@ -0,0 +1,35 @@
+package supplychain.simulator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by rene on 01.09.14.
+ */
+public class WeatherNCDCProvider {
+
+    // HTTP GET request
+    public String get(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("token", "FHZLEZWsBusSZulVeicGwUzkXTCFTBIp");
+
+        int responseCode = con.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //System.out.println(response.toString());
+        return response.toString();
+    }
+}
