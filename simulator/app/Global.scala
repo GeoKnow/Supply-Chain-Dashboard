d24e3eb4daeded2068a5b43b0511c33895d4b2d7
Rene Pietzsch <rpietzsch@brox.de>
2015-09-14 12:21:30 +0200

    adds CORES header

--- simulator/app/Global.scala
+++ simulator/app/Global.scala
@@ -1,6 +1,19 @@
+import play.api.mvc.{Result, RequestHeader, Filter, WithFilters}
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
  * Created by rene on 14.09.15.
  */
-class Global {
+object Global extends WithFilters(CorsFilter) {
 
+}
+
+object CorsFilter extends Filter {
+  def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
+    for(res <- next(request)) yield {
+      res.withHeaders(("Access-Control-Allow-Origin", "*"))
+    }
+  }
 }
